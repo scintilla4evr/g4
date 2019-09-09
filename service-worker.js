b2b2b119f994466ea27e449ad595d0b00c2e0a54
Matthew Garbett <LinkofHyruleUO@gmail.com>
2019-09-09 11:12:02 -0600

    More work on Service worker

--- service-worker.js
+++ service-worker.js
@@ -4,7 +4,7 @@ const CACHE_NAME = 'static-cache-v1';
 const DATA_CACHE_NAME = 'data-cache-v1';
 
 const FILES_TO_CACHE = [
-    '/',
+    ' ',
     'index.html',
     'assets/music.mp3',
     'scripts/game.js',
