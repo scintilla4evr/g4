781a38f60c027bae1f992b64ca085edd1ae0a914
dynamyc | Bálint Sebestyén <33225256+dynamyc010@users.noreply.github.com>
2019-09-12 22:22:02 +0200

    Made discordIntegration.js work on startup.
    uwu it works

--- scripts/discordIntegration.js
+++ scripts/discordIntegration.js
@@ -3,6 +3,20 @@ let electron = require("electron")
 let discord = electron.remote.require("discord-rpc")
 let clientId = "620780964494442500"
 
+// dynamyc's section
+let currentMode = "Normal"
+let currentScore = "0"
+
+function sleep(milliseconds) {
+    var start = new Date().getTime();
+    for (var i = 0; i < 1e7; i++) {
+      if ((new Date().getTime() - start) > milliseconds){
+        break;
+      }
+    }
+  }
+
+
 const fs = require('fs');
 const clientID = JSON.parse(fs.readFileSync(__dirname + "/discordID.json", "utf-8")).clientId
 let path  = require("path")
@@ -13,8 +27,12 @@ const rpc = new discord.Client({
     transport: "ipc"
 })
 
+rpc.login({
+    clientId: clientId
+})
+
 window.addEventListener("g4statechange", (e) => {
-	let modeAlias = {
+    let modeAlias = {
 		easy: "Easy",
 		normal: "Normal",
 		hard: "Hard",
@@ -22,11 +40,29 @@ window.addEventListener("g4statechange", (e) => {
 		hades: "Hades",
 		denise: "Chaos",
 		reverse: "Reverse"
-	}
-	
+    }
+    currentMode = modeAlias[e.detail.mode]
+    currentScore = e.detail.levelIndex
+
     let activityData = {
-        details: "Playing on: " + modeAlias[e.detail.mode],
-        state: "Score: " + e.detail.levelIndex,
+        details: "Playing on: " + currentMode,
+        state: "Score: " + currentScore,
+
+        largeImageKey: "g4_logo",
+        largeImageText: "G4",
+        smallImageKey: "g4",
+
+        instance: false
+    }
+
+    rpc.setActivity(activityData)
+
+})
+
+function setRPC() {
+    let activityData = {
+        details: "Playing on: " + currentMode,
+        state: "Score: " + currentScore,
 
         largeImageKey: "g4_logo",
         largeImageText: "G4",
@@ -36,8 +72,9 @@ window.addEventListener("g4statechange", (e) => {
     }
     
     rpc.setActivity(activityData)
-})
+}
 
-rpc.login({
-    clientId: clientId
-})
+sleep(250)
+setRPC()
+
+
