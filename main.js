f8b3c3266ab3a633c16afb070b5b6f250f52d808
scintilla4evr <kacper17@outlook.com>
2019-09-28 15:12:49 +0200

    Fixed the titlebar unresponsiveness

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, ipcMain } = require('electron');
 const fs = require('fs');
 const path  = require("path");
 
@@ -11,7 +11,7 @@ function createWindow () {
 
     frame: false,
     icon: __dirname + '/icon.ico',
-    
+
     webPreferences: {
       nodeIntegration: true
     }
@@ -27,6 +27,35 @@ function createWindow () {
         ), true
     )
   })
+
+  initIpcEvents(win)
+}
+
+/**
+ * @param {BrowserWindow} win 
+ */
+function initIpcEvents(win) {
+  ipcMain.on("minimize", (e) => {
+    win.minimize()
+  })
+  ipcMain.on("maximize", (e) => {
+    win.maximize()
+  })
+  ipcMain.on("unmaximize", (e) => {
+    win.unmaximize()
+  })
+  ipcMain.on("close", (e) => {
+    win.close()
+  })
+
+  if (win.isMaximized()) win.webContents.send("maximized")
+
+  win.on("maximize", () => {
+    win.webContents.send("maximized")
+  })
+  win.on("unmaximize", () => {
+    win.webContents.send("unmaximized")
+  })
 }
 
 app.commandLine.appendSwitch("autoplay-policy", "no-user-gesture-required");
@@ -35,4 +64,6 @@ app.on('ready', createWindow);
 app.on('browser-window-created', (e, window) => {
   window.setMenu(null);
 });
-
+app.on('window-all-closed', () => {
+  app.exit()
+})
--- scripts/electronUI.js
+++ scripts/electronUI.js
@@ -1,30 +1,28 @@
+const {ipcRenderer} = require("electron")
 const {BrowserWindow} = require("electron").remote
 
 document.body.classList.add("electron")
 
-let win = BrowserWindow.getFocusedWindow()
-if (win.isMaximized()) document.body.classList.add("maximized")
-
 document.querySelector("button#minimizeBtn").addEventListener("click", () => {
-    win.minimize()
+    ipcRenderer.send("minimize")
 })
 
 document.querySelector("button#maximizeBtn").addEventListener("click", () => {
-    win.maximize()
+    ipcRenderer.send("maximize")
 })
 
 document.querySelector("button#restoreBtn").addEventListener("click", () => {
-    win.unmaximize()
+    ipcRenderer.send("unmaximize")
 })
 
 document.querySelector("button#closeBtn").addEventListener("click", () => {
-    win.close()
+    ipcRenderer.send("close")
 })
 
-win.on("maximize", () => {
+ipcRenderer.on("maximized", (e) => {
     document.body.classList.add("maximized")
 })
 
-win.on("unmaximize", () => {
+ipcRenderer.on("unmaximized", (e) => {
     document.body.classList.remove("maximized")
 })
